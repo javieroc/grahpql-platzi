94316b1af6df435ca1abb5382788fca7bdd656ce
Javier Ocampo <ferroxido@gmail.com>
2017-08-28 14:47:13 -0300

    Schema refactor

---
+++ schema/Curso.js
@@ -0,0 +1,17 @@
+module.exports = `
+  # Esto es un curso en el sistema
+  type Curso {
+    id: ID!
+    titulo: String!
+    descripcion: String!
+    profesor: Profesor
+    rating: Float
+    comentarios: [Comentario]
+  }
+
+  type Comentario {
+    id: ID!
+    nombre: String!
+    cuerpo: String!
+  }
+`
